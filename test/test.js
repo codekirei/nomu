2ae4824da6ddcf3d7be226e99599b8e02740a3e9
Jacob Blakely <jacob@codekirei.com>
2015-12-07 17:56:26 -0800

    add todo

--- test/test.js
+++ test/test.js
@@ -1,10 +1,24 @@
 'use strict'
 
 const nm = require('../index')
+const co = require('co')
 
 const rename = path => path.replace('test', './dist')
 
-nm.read('test/**/*.js')
-  .then(nm.rename.bind(rename))
-  .then(nm.debug)
-  .then(nm.write)
+// function* main() {
+//   const data = yield nm.read('test/**/*.js')
+//     .then(nm.debug)
+//   console.log(data)
+// }
+
+function main() {
+  nm.read('test/**/*.js')
+    .then(console.log)
+}
+
+co(main())
+
+// nm.read('test/**/*.js')
+//   .then(nm.rename.bind(rename))
+//   .then(nm.debug)
+//   .then(nm.write)
